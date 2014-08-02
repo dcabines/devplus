3c67df0d62ed39381599e27765df81bc33dbb94f
David Cabiness <dcabines@gmail.com>
2014-08-02 14:53:24 -0400

    .

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ app.configure(function () {
     app.use(app.router);
     app.use(require('less-middleware')({ src: __dirname + '/public' }));
     app.use(express.static(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'bower_components')));
 });
 
 app.configure('development', function () {
