dad24658f608df56ca012492920e8a4a8d817b14
David Cabiness <dcabines@gmail.com>
2014-08-03 17:04:33 -0400

    .

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+var expressBundles = require('express-bundles');
 var home = require('./routes/home');
 
 var app = express();
@@ -8,8 +9,28 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(require('less-middleware')(__dirname + '/public'));
+
+app.use(expressBundles.middleware({
+  env: app.get('env'),
+  src: path.join(__dirname, 'public'),
+  bundles: {
+    'js/bundle.js': [
+      'components/angular/angular.js',
+      'components/angular-ui/build/angular-ui.js',
+      'components/angular-ui-router/release/angular-ui-router.js'
+    ],
+    'js/app-bundle.js': [
+      'js/app.js',
+      'js/services.js',
+      'js/controllers.js',
+      'js/filters.js',
+      'js/directives.js'
+    ]
+  }
+}));
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/*', home);
+app.use('/', home);
 
 
 app.listen(app.get('port'), function () {
