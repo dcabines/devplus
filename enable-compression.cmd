ae6381c2f5e3cc6696a3e67d1035a23a482fa940
David Cabiness <dcabines@gmail.com>
2014-08-04 22:49:36 -0400

    .

--- enable-compression.cmd
+++
@@ -1,2 +0,0 @@
-%windir%\system32\inetsrv\appcmd set config /section:urlCompression /doDynamicCompression:True /commit:apphost
-%windir%\system32\inetsrv\appcmd set config  -section:system.webServer/httpCompression /+"dynamicTypes.[mimeType='application/json; charset=utf-8',enabled='True']" /commit:apphost
